b5f39ff2049b3ee7fd351b06efc690ff286fb757
maxvdkelft0 <m.van.de.kelft@sintlucasedu.nl>
2024-05-31 10:38:57 +0200

    Added: Edit / Remove Expense
    Added: functionality to edit / delete a expense.

--- app.js
+++ app.js
@@ -1,29 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Sample data (can be replaced with real data from backend)
     let sampleExpenses = JSON.parse(localStorage.getItem('expenses')) || [];
 
     function saveExpensesToLocalStorage() {
         localStorage.setItem('expenses', JSON.stringify(sampleExpenses));
     }
 
-    // Login form submission event listener
     const loginForm = document.getElementById('login-form');
     if (loginForm) {
         loginForm.addEventListener('submit', function (event) {
             event.preventDefault();
-            console.log('test')
             const username = document.getElementById('username').value;
             const password = document.getElementById('password').value;
 
-            // Check login credentials
             if (username === 'admin' && password === 'admin') {
                 showToast('U bent succesvol ingelogd!', 'success');
                 setTimeout(() => {
-                    window.location.href = 'index.html'; // Redirect after showing toast
-                }, 1000); // Delay to show the toast before redirecting
+                    window.location.href = 'index.html';
+                }, 1000);
             } else {
                 showToast('Ongeldige gebruikersnaam of wachtwoord.', 'error');
-                // Clear input fields
                 document.getElementById('username').value = '';
                 document.getElementById('password').value = '';
             }
@@ -45,7 +40,6 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-    // Event listener for password toggle button
     const passwordToggle = document.getElementById('password-toggle');
     if (passwordToggle) {
         passwordToggle.addEventListener('click', function () {
@@ -53,21 +47,16 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
-    // Event listener for logout button
     const logoutButton = document.querySelector('.logout-button');
     if (logoutButton) {
         logoutButton.addEventListener('click', function () {
-            // Show logout toast
             showToast('U bent succesvol uitgelogd!', 'success');
-            // Redirect to login page after showing the toast
             setTimeout(() => {
                 window.location.href = 'login.html';
-            }, 2000); // Delay before redirecting
+            }, 2000);
         });
     }
 
-
-    // Function to update dashboard
     function updateDashboard() {
         const dashboardTable = document.getElementById('dashboard-table');
         dashboardTable.innerHTML = '';
@@ -78,7 +67,6 @@ document.addEventListener('DOMContentLoaded', function () {
             noExpensesMessage.textContent = 'Voeg eerst uitgaven toe om dashboard te zien.';
             dashboardTable.appendChild(noExpensesMessage);
         } else {
-            // Calculate total expenses for each category
             const categoryTotals = {};
             sampleExpenses.forEach(expense => {
                 if (!categoryTotals[expense.category]) {
@@ -87,10 +75,8 @@ document.addEventListener('DOMContentLoaded', function () {
                 categoryTotals[expense.category] += expense.amount;
             });
 
-            // Sort categories by total expenses
             const sortedCategories = Object.keys(categoryTotals).sort((a, b) => categoryTotals[b] - categoryTotals[a]);
 
-            // Display top 5 categories in the dashboard table
             dashboardTable.innerHTML = `
                 <thead>
                     <tr>
@@ -112,41 +98,54 @@ document.addEventListener('DOMContentLoaded', function () {
             }
             dashboardTable.innerHTML += `</tbody>`;
             saveExpensesToLocalStorage();
-
         }
     }
 
     function addExpenseToList(expense) {
         const expenseList = document.getElementById('expense-list');
 
-        // Remove "Geen uitgaven toegevoegd." message if it exists
         const noExpensesMessage = document.querySelector('.no-expenses');
         if (noExpensesMessage) {
             noExpensesMessage.remove();
         }
 
         const listItem = document.createElement('li');
-        listItem.className = 'category-item fade-in'; // Add animation class
+        listItem.className = 'category-item fade-in';
 
         const expenseText = document.createElement('span');
         expenseText.textContent = `${expense.category}: €${expense.amount}`;
-        expenseText.className = 'category-name'; // Add class for styling
+        expenseText.className = 'category-name';
 
         const expenseTime = document.createElement('span');
         const formattedDate = new Date(expense.timestamp).toLocaleString('nl-NL');
-        expenseTime.innerHTML = `<i class="fas fa-clock"></i> ${formattedDate}`;
-        expenseTime.className = 'expense-time'; // Add class for styling
+        expenseTime.innerHTML = `<i class="mr-2 fas fa-clock"></i> ${formattedDate}`;
+        expenseTime.className = 'expense-time';
+
+        const editButton = document.createElement('button');
+        editButton.innerHTML = '<i class="fas fa-edit"></i>';
+        editButton.className = 'edit-button text-yellow-500 bg-yellow-500/20 hover:bg-yellow-500/50 hover:text-white transition-all px-2 py-1 rounded-md';
+        editButton.addEventListener('click', function () {
+            openEditModal(expense);
+        });
+
+        const deleteButton = document.createElement('button');
+        deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+        deleteButton.className = 'delete-button text-red-500 bg-red-500/20 hover:bg-red-500/50 hover:text-white transition-all px-2 py-1 rounded-md';
+        deleteButton.addEventListener('click', function () {
+            deleteExpense(expense);
+        });
 
         listItem.appendChild(expenseText);
         listItem.appendChild(expenseTime);
-        expenseList.prepend(listItem); // Use prepend instead of appendChild
+        listItem.appendChild(editButton);
+        listItem.appendChild(deleteButton);
+        expenseList.prepend(listItem);
         saveExpensesToLocalStorage();
     }
 
-    // Define the displayExpenses function
     function displayExpenses() {
         const expenseList = document.getElementById('expense-list');
-        expenseList.innerHTML = ''; // Clear previous content
+        expenseList.innerHTML = '';
         sampleExpenses = JSON.parse(localStorage.getItem('expenses')) || [];
         if (sampleExpenses.length === 0) {
             const noExpensesMessage = document.createElement('li');
@@ -160,14 +159,11 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-
-    // Function to show toast messages
     function showToast(message, type) {
         const toastContainer = document.getElementById('toast-container');
         const toast = document.createElement('div');
         toast.className = `toast px-4 py-2 rounded-md text-white shadow-sm ${type === 'success' ? 'toast-success' : 'toast-error'}`;
 
-        // Add icon based on the type of message
         const icon = document.createElement('i');
         icon.className = `fas ${type === 'success' ? 'fa-check-circle' : 'fa-exclamation-circle'} mr-2`;
 
@@ -178,51 +174,98 @@ document.addEventListener('DOMContentLoaded', function () {
 
         setTimeout(() => {
             toast.classList.add('show');
-        }, 100); // Add delay to trigger transition
+        }, 100);
 
         setTimeout(() => {
             toast.classList.remove('show');
             toast.classList.add('hide');
             setTimeout(() => {
                 toast.remove();
-            }, 500); // Wait for the fade out transition to finish
-        }, 3000); // Duration to show the toast
+            }, 500);
+        }, 3000);
     }
 
-    // Display initial expenses and dashboard
-    displayExpenses();
-    updateDashboard();
+    function openEditModal(expense) {
+        const editModal = document.getElementById('edit-modal');
+        const editAmount = document.getElementById('edit-amount');
+        const editCategory = document.getElementById('edit-category');
 
-    // Form submission event listener
+        editAmount.value = expense.amount;
+        editCategory.value = expense.category;
+
+        editModal.dataset.id = expense.id;
+        editModal.style.display = 'flex';
+    }
+
+    function closeEditModal() {
+        const editModal = document.getElementById('edit-modal');
+        editModal.style.display = 'none';
+    }
+
+    function saveEditedExpense() {
+        const editModal = document.getElementById('edit-modal');
+        const editAmount = document.getElementById('edit-amount').value;
+        const editCategory = document.getElementById('edit-category').value;
+        const expenseId = editModal.dataset.id;
+
+        const expenseIndex = sampleExpenses.findIndex(exp => exp.id === expenseId);
+        if (expenseIndex !== -1) {
+            sampleExpenses[expenseIndex].amount = parseFloat(editAmount);
+            sampleExpenses[expenseIndex].category = editCategory;
+
+            saveExpensesToLocalStorage();
+            displayExpenses();
+            updateDashboard();
+            showToast('Uitgave bijgewerkt.', 'success');
+        } else {
+            showToast('Fout bij het bijwerken van de uitgave.', 'error');
+        }
+
+        closeEditModal();
+    }
+
+    function deleteExpense(expense) {
+        sampleExpenses = sampleExpenses.filter(exp => exp.id !== expense.id);
+        saveExpensesToLocalStorage();
+        displayExpenses();
+        updateDashboard();
+        showToast('Uitgave verwijderd.', 'success');
+    }
     const expenseForm = document.getElementById('expense-form');
     expenseForm.addEventListener('submit', function (event) {
         event.preventDefault();
 
-        const amount = parseFloat(document.getElementById('amount').value);
-        const category = document.getElementById('category').value;
+        const amountInput = document.getElementById('amount');
+        const categoryInput = document.getElementById('category');
 
-        // Validation
-        if (isNaN(amount) || amount <= 0 || category.trim() === '') {
-            showToast('Voer alstublieft een geldig bedrag en categorie in.', 'error');
-            return;
+        const amount = parseFloat(amountInput.value);
+        const category = categoryInput.value;
+
+        if (!amount || !category) {
+            showToast('Vul alle velden in!', 'error');
+            return; // Exit the function if either amount or category is missing
         }
 
-        const newExpense = { amount, category, timestamp: new Date() };
+        const newExpense = {
+            id: Date.now().toString(),
+            amount: amount,
+            category: category,
+            timestamp: new Date().toISOString()
+        };
 
-        // Nieuwe uitgave toevoegen aan de lijst
         sampleExpenses.push(newExpense);
-
-        // Dashboard bijwerken
-        updateDashboard();
-
-        // Nieuwe uitgave weergeven met animatie
         addExpenseToList(newExpense);
-
-        // Show success toast
-        showToast('Uitgave van €' + amount + ' voor ' + category + ' toegevoegd.', 'success');
-
-        // Formulier resetten
+        updateDashboard();
+        showToast('Uitgave van €' + amount + ' voor ' + category + ' is toegevoegd.', 'success');
         expenseForm.reset();
     });
 
+
+    const saveEditButton = document.getElementById('save-edit-button');
+    const cancelEditButton = document.getElementById('cancel-edit-button');
+    saveEditButton.addEventListener('click', saveEditedExpense);
+    cancelEditButton.addEventListener('click', closeEditModal);
+
+    displayExpenses();
+    updateDashboard();
 });
